6282f447ad92f32e701c55f86caf86d73af585c3
s_yamaga <s_yamaga@codmon.co.jp>
2023-11-18 11:27:27 +0900

    wip

--- api/src/main/kotlin/com/example/attendanceapi/domain/model/Attendance.kt
+++ api/src/main/kotlin/com/example/attendanceapi/domain/model/Attendance.kt
@@ -1,5 +1,9 @@
 package com.example.attendanceapi.domain.model
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import com.example.attendanceapi.gateway.api.BreakRecord
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
@@ -37,11 +41,36 @@ class Attendances(val list: List<Attendance>) {
         Attendances(list.filter { it.onDate(date) })
 
     fun filterByDateAndKind(date: LocalDate, kind: AttendanceKind): Attendance? =
-        this.filterByDate(date).list.find{ it -> it.kind == kind}
+        this.filterByDate(date).list.find { it -> it.kind == kind }
 
     fun splitIntoDailyAttendances(): List<Pair<String, List<Attendance>>> =
         list.groupBy { it.dateTime.toLocalDate().toString() }.map { Pair(it.key, it.value) }
 }
+
+class DailyAttendance(val date: LocalDate, val attendances: List<Attendance>) {
+
+
+    fun createBreakCords(): BreakRecords? {
+        val breaks = this.attendances.filter { it.kind == AttendanceKind.LEAVE || it.kind == AttendanceKind.END }
+        breaks.sortedWith(compareBy { it.dateTime })
+    }
+}
+
+class BreakRecords(val breakRecords: List<com.example.attendanceapi.gateway.api.BreakRecord>)
+
+class BreakRecord private constructor(pair: Pair<Attendance, Attendance>) {
+    companion object {
+        fun of(pair: Pair<Attendance, Attendance>) {
+            if (pair.first.kind == AttendanceKind.LEAVE || pair.second.kind == AttendanceKind.BACK) {
+                BreakRecord(pair).right()
+            } else {
+                BreakRecordError("").left()
+            }
+        }
+    }
+}
+
+data class BreakRecordError(val message: String)
 
 sealed interface AttendancesFilterByDateError {
     data class NotFound(val input: LocalDate, val message: String) : AttendancesFilterByDateError
--- api/src/test/kotlin/com/example/attendanceapi/domain/model/AttendanceSpec.kt
+++ api/src/test/kotlin/com/example/attendanceapi/domain/model/AttendanceSpec.kt
@@ -1,4 +1,33 @@
 package com.example.attendanceapi.domain.model
 
-class AttendanceSpec {
-}
+import io.kotest.core.spec.style.DescribeSpec
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Month
+import java.util.*
+
+class AttendanceSpec: DescribeSpec({
+    describe("DailyAttendance.createBreakCords"){
+        fun attendance_20230401(hour: Int, minutes: Int, kind: AttendanceKind) =
+            Attendance(
+                UUID.randomUUID(),
+                "",
+                LocalDateTime.of(2023, Month.APRIL, 1, hour, minutes),
+                "leave",
+                kind
+            )
+
+        it("BreakRecordsを生成する"){
+            //given
+            DailyAttendance(
+                date = LocalDate.of(2023, Month.APRIL, 1),
+                attendances = listOf(
+                    attendance_20230401(12, 0, AttendanceKind.LEAVE),
+                    attendance_20230401(12, 0, AttendanceKind.BACK),
+                    attendance_20230401(12, 0, AttendanceKind.LEAVE),
+                    attendance_20230401(12, 0, AttendanceKind.BACK)
+                )
+            )
+        }
+    }
+})
